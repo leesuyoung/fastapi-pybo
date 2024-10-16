24565250ecd94d7ba0e592c52e0f72b43bf47197
이수영 <leesuyoung1@gmail.com>
2024-10-16 19:13:37 +0900

    FastAPI with Dash application example.(modify)

--- frontend/src/routes/DashIframe.svelte
+++ frontend/src/routes/DashIframe.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="container">
-    <iframe src="http://127.0.0.1:8000/dash/" width="100%" height="600" title="Dash iframe"></iframe>
+    <iframe src="http://3.39.95.150:8000/dash/" width="100%" height="600" title="Dash iframe"></iframe>
 </div>
